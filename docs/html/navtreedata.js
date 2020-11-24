73cc6cab12788ac996a79b1c803b61846416e390
Michael Staneker <50531288+MichaelSt98@users.noreply.github.com>
2020-11-24 23:00:03 +0100

    Updated Documentation.

---
+++ docs/html/navtreedata.js
@@ -0,0 +1,96 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Cpp concept project", "index.html", [
+    [ "C++ concepts project", "index.html", [
+      [ "Idea", "index.html#autotoc_md1", null ],
+      [ "Related documents", "index.html#autotoc_md2", null ],
+      [ "Structure", "index.html#autotoc_md3", [
+        [ "Folders", "index.html#autotoc_md4", null ]
+      ] ],
+      [ "Content (Concepts)", "index.html#autotoc_md5", [
+        [ "Programming concepts", "index.html#autotoc_md6", null ],
+        [ "Documentation", "index.html#autotoc_md7", null ],
+        [ "Makefile", "index.html#autotoc_md8", null ]
+      ] ]
+    ] ],
+    [ "Markdown cheatsheet", "md_documents__markdown.html", [
+      [ "Header 1", "md_documents__markdown.html#autotoc_md10", [
+        [ "Header 2", "md_documents__markdown.html#autotoc_md11", [
+          [ "Header 3", "md_documents__markdown.html#autotoc_md12", [
+            [ "Header 4", "md_documents__markdown.html#autotoc_md13", null ]
+          ] ]
+        ] ]
+      ] ],
+      [ "Emphasis", "md_documents__markdown.html#autotoc_md15", null ],
+      [ "Lists", "md_documents__markdown.html#autotoc_md16", null ],
+      [ "Links", "md_documents__markdown.html#autotoc_md17", null ],
+      [ "Images", "md_documents__markdown.html#autotoc_md18", null ],
+      [ "Code and Syntax Highlighting", "md_documents__markdown.html#autotoc_md19", null ],
+      [ "Tables", "md_documents__markdown.html#autotoc_md20", null ],
+      [ "Blockquotes", "md_documents__markdown.html#autotoc_md21", null ],
+      [ "Inline HTML", "md_documents__markdown.html#autotoc_md22", null ],
+      [ "Horizontal", "md_documents__markdown.html#autotoc_md23", null ],
+      [ "YouTube Videos", "md_documents__markdown.html#autotoc_md25", null ]
+    ] ],
+    [ "Project structure", "md_documents_structure.html", [
+      [ "Folders", "md_documents_structure.html#autotoc_md27", null ],
+      [ "Files", "md_documents_structure.html#autotoc_md28", null ]
+    ] ],
+    [ "Unit-Tests", "md_documents__unit-_tests.html", [
+      [ "Integrated in CLion", "md_documents__unit-_tests.html#autotoc_md30", [
+        [ "Google Test", "md_documents__unit-_tests.html#autotoc_md31", null ],
+        [ "Catch", "md_documents__unit-_tests.html#autotoc_md32", null ],
+        [ "Boost.Test", "md_documents__unit-_tests.html#autotoc_md33", null ],
+        [ "Doctest", "md_documents__unit-_tests.html#autotoc_md34", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_concept_class_8cpp.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
